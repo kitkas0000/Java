6c854d0f78dbb8ec3389e6e75290f1b97d8777b8
NecoPC <ducndhe171600@fpt.edu.vn>
2023-04-06 15:09:38 +0700

    Init

---
+++ CarPrj/src/CarPrj/Classes/Com/Brand.java
@@ -0,0 +1,39 @@
+package CarPrj.Classes.Com;
+
+public class Brand {
+
+    String brandID;
+    String brandName;
+    private String soundBrand;
+    private double price;
+
+    Brand () {
+
+    }
+
+    Brand (String brandID, String brandName, String soundBrand, double price) {
+        this.brandID = brandID;
+        this.brandName = brandName;
+        this.soundBrand = soundBrand;
+        this.price = price;
+    }
+    
+    public String getBrandID () {
+        return brandID;
+    }
+
+    public String getBrandName () {
+        return brandName;
+    }
+
+    public void setUpdatedBrand (String brandName, String soundBrand, double price) {
+        this.brandName = brandName;
+        this.soundBrand = soundBrand;
+        this.price = price;
+    }
+
+    @Override
+    public String toString () {
+        return brandID + "," + brandName + "," + soundBrand + ":" + price; 
+    }
+}
